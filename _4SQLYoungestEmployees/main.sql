0bc7ada4b0d590102824de360a3a4394017ec9ea
Stévillis Sousa <stevillis@hotmail.com>
2022-02-18 16:22:26 -0400

    SQL Youngest Employees

---
+++ _4SQLYoungestEmployees/main.sql
@@ -0,0 +1,202 @@
+/*
+<div id="main-splitpane-left" class="coding-question__left-pane"><section class="question-view__title-wrapper"><div class="beta-icon-editor beta-desc">BETA</div><span class="question-view__switch-theme__wrapper">Can’t read the text? <button class="ui-btn ui-btn-normal ui-btn-plain btn-as-link question-view__switch-theme"><div class="ui-content align-icon-right"><span class="ui-text" aria-hidden="false">Switch</span></div></button> theme</span><h1 class="question-view__title">4. Youngest Employees</h1></section><section class="question-view__instruction"><div class="candidate-rich-text"><div id="aiimr64j100-instruction"><style type="text/css">.ps-content-wrapper-v0 div { margin: 0 auto; overflow: auto; } .ps-content-wrapper-v0 div.preheader { display: none; } .ps-content-wrapper-v0 p { white-space: pre-wrap; padding-left: 4px; padding-right: 4px; padding-top: 0px; padding-bottom: 2px; } .ps-content-wrapper-v0 p.section-title { font-weight: bold; padding-bottom: 0px; } .ps-content-wrapper-v0 ol.plain-list, .ps-content-wrapper-v0 ul.plain-list { list-style-type: none; padding: 4px; } .ps-content-wrapper-v0 li { white-space: normal; margin-top: 4px; margin-bottom: 4px; } .ps-content-wrapper-v0 code { color: black; background: transparent; font-weight: normal; font-size: 1em; } .ps-content-wrapper-v0 pre { background-color: #f4faff; border: 0; border-radius: 2px; margin: 8px; padding: 10px; } .ps-content-wrapper-v0 pre.scrollable-full-json { overflow-x: scroll; white-space: pre; } .ps-content-wrapper-v0 pre.scrollable-json { height: 300px; overflow-y: scroll; display: inline-grid; white-space: pre-wrap; padding-left: 8px; padding-right: 8px; padding-top: 4px; padding-bottom: 4px; } .ps-content-wrapper-v0 div.equation-parent { width: 500px; text-align: center; border: 1px solid #000; padding: 4px; } .ps-content-wrapper-v0 div.equation-parent.equation { width: 100%; display: inline-block; } .ps-content-wrapper-v0 figure { background-color: transparent; display: table; margin-top: 8px; margin-bottom: 8px; text-align: center; margin-left: auto; margin-right: auto; } .ps-content-wrapper-v0 figcaption { text-align: center; display: table-caption; caption-side: bottom; margin-top: 4px; margin-bottom: 4px; } .ps-content-wrapper-v0 img { width: auto; max-width: 100%; height: auto; } .ps-content-wrapper-v0 details { background-color: transparent; padding-left: 4px; padding-right: 4px; padding-top: 0px; padding-bottom: 2px; } .ps-content-wrapper-v0 details details { padding-left: 8px; padding-right: 8px; } .ps-content-wrapper-v0 details summary { background-color: #39424e; color: white; font-weight: bold; margin-top: 4px; margin-bottom: 4px; padding: 8px; } .ps-content-wrapper-v0 details details summary code { color: white; font-weight: bold; padding-left: 2px; padding-right: 2px; padding-top: 4px; padding-right: 4px; margin-left: 4px; } .ps-content-wrapper-v0 details div.collapsable-details { margin: 0 auto; padding-left: 4px; padding-right: 4px; padding-top: 0px; padding-bottom: 2px; overflow: auto; } .ps-content-wrapper-v0 details div.collapsable-details pre { margin-left: 4px; margin-right: 4px; margin-top: 4px; margin-bottom: 4px; } .ps-content-wrapper-v0 table.normal { border: 1px solid black; border-collapse: collapse; border-color: darkgray; margin: 0 auto; margin-top: 8px; margin-bottom: 8px; padding: 8px; width: 96%; table-layout: fixed; } .ps-content-wrapper-v0 table.normal tbody { display: block; overflow-x: auto; overflow-y: hidden; } .ps-content-wrapper-v0 table.normal tbody tr:first-child th { font-weight: bold; white-space: normal; } .ps-content-wrapper-v0 table.normal tbody tr th, .ps-content-wrapper-v0 table.normal tbody tr td { font-weight: normal; white-space: nowrap; text-align: center; vertical-align: middle; border: 1px solid black; border-color: darkgray; padding: 8px; } .ps-content-wrapper-v0 table.database-table { border-collapse: collapse; border-color: darkgray; border: 1px solid black; width: auto; margin-left: 4px; margin-top: 8px; margin-bottom: 8px; padding: 8px; } .ps-content-wrapper-v0 table.database-table tbody { overflow-x: auto; overflow-y: hidden; border: none; } .ps-content-wrapper-v0 table.database-table tbody tr th, .ps-content-wrapper-v0 table.database-table tbody tr td { font-weight: normal; white-space: nowrap; text-align: center; vertical-align: middle; border: 1px solid black; border-color: darkgray; padding: 8px; } .ps-content-wrapper-v0 table.database-table tbody tr th { font-weight: bold; border: 1px solid black; } .ps-content-wrapper-v0 table.database-table tbody tr:nth-child(2) td { border: 1px solid black; } .ps-content-wrapper-v0 table.database-table tbody tr:nth-child(n+2) td:first-child { border-left-color: black; } .ps-content-wrapper-v0 table.database-table tbody tr:nth-child(n+2) td:last-child { border-right-color: black; } .ps-content-wrapper-v0 table.database-table tbody tr:last-child td { border-bottom-color: black; } .ps-content-wrapper-v0 table.database-table tbody tr td.description { text-align: left; white-space: pre-wrap; } .ps-content-wrapper-v0 table.normal tbody tr th.description { width: 60%; } .ps-content-wrapper-v0 table.function-params tbody tr:first-child td.headers { border-bottom-width: 2px; } .ps-content-wrapper-v0 table.function-params tbody tr:last-child td { border-top-width: 2px; border-top-color: darkgray; } .ps-content-wrapper-v0 table.function-params tbody tr td.headers { width: 25%; font-weight: bold; text-align: center; border: 1px solid black; border-right-width: 2px; border-color: darkgray; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell { width: 100%; height: 100%; padding: 0px; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table { width: 100%; height: 100%; padding: 0px; margin: 0px; border: 0; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr td.code { white-space: normal; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr th { border-top: 0; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr th:first-child { border-left: 0; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr th:last-child { border-right: 0; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr:last-child td { border-bottom: 0; border-top-width: 1px; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr td:first-child { border-left: 0; } .ps-content-wrapper-v0 table.function-params tbody tr td.params-table-cell table.params-table tbody tr td:last-child { border-right: 0; } .ps-content-wrapper-v0 table.sudoku { border-collapse: collapse; border-color: darkgray; margin: 0 auto; margin-top: 8px; margin-bottom: 8px; padding: 8px; } .ps-content-wrapper-v0 table.sudoku colgroup, tbody { border: 3px solid black; } .ps-content-wrapper-v0 table.sudoku td { border: 1px solid black; height: 25px; width: 25px; text-align: center; padding: 0; } .ps-content-wrapper-v0 .left { text-align: left; } .ps-content-wrapper-v0 .right { text-align: right; } .ps-content-wrapper-v0 .code { font-family: monospace; white-space: nowrap; } .ps-content-wrapper-v0 .json-object-array ol, .ps-content-wrapper-v0 .json-object-array ol ul { margin-top: 0px; padding-left: 14px; } .json-object-array li { float: left; margin-right: 30px; margin-left: 10px; } .json-object-array pre { padding: 4px; margin-left: 0px; }
+</style>
+<div class="ps-content-wrapper-v0">
+<p style="font-style: normal;font-variant: normal;font-size: 16px;">There are two data tables with employee information:&nbsp;<em>EMPLOYEE</em>&nbsp;and <em>EMPLOYEE_UIN</em>. Query the tables to generate a list of all employees who are less than <em>25</em>&nbsp;years old first in order of <em>NAME</em>, then of <em>ID</em>, both ascending.&nbsp; The result should include the <em>UIN</em>&nbsp;followed by the&nbsp;<em>NAME.&nbsp;&nbsp;</em></p>
+
+<p style="font-style: normal;font-variant: normal;font-size: 16px;">&nbsp;</p>
+
+<p style="font-style: normal;font-variant: normal;font-size: 16px;"><strong>Note:</strong>&nbsp; While the secondary sort is by <em>ID</em>, the result includes <em>UIN</em> but not <em>ID</em>.</p>
+
+<p>&nbsp;</p>
+
+<details open=""><summary class="section-title">Schema</summary>
+
+<div class="collapsable-details">
+<table class="database-table">
+	<tbody>
+		<tr>
+			<th colspan="100">EMPLOYEE</th>
+		</tr>
+		<tr>
+			<th>Name</th>
+			<th>Type</th>
+			<th>Description</th>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">ID</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Integer</td>
+			<td>The ID of the employee. This is a primary key.</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">NAME</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">String</td>
+			<td>The name of the employee having <em>[1, 20]</em> characters.</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">AGE</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Integer &nbsp;&nbsp;</td>
+			<td>The age of the employee.</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">ADDRESS</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">String</td>
+			<td>The address of the employee having <em>[1, 25]</em> characters.</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">SALARY</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Integer</td>
+			<td>The salary of the employee.</td>
+		</tr>
+	</tbody>
+</table>
+
+<table class="database-table">
+	<tbody>
+		<tr>
+			<th colspan="100">EMPLOYEE_UIN</th>
+		</tr>
+		<tr>
+			<th>Name</th>
+			<th>Type</th>
+			<th>Description</th>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">ID</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Integer</td>
+			<td>The ID of the employee. This is a primary key.</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">UIN</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">String</td>
+			<td>The unique identification number of the employee.</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+</details>
+
+<details open=""><summary class="section-title">Sample Data Tables</summary>
+
+<div class="collapsable-details">
+<p><strong>Sample Input</strong></p>
+
+<table class="database-table">
+	<tbody>
+		<tr>
+			<th colspan="99">EMPLOYEE</th>
+		</tr>
+		<tr>
+			<th style="font-family:monospace;">ID</th>
+			<th style="font-family:monospace;">NAME</th>
+			<th style="font-family:monospace;">AGE</th>
+			<th style="font-family:monospace;">ADDRESS</th>
+			<th style="font-family:monospace;">SALARY</th>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">1</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Sherrie</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">23</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Paris</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">74635</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">2</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Paul</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">30</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Sydney</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">72167</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">3</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Mary</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">24</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Paris</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">75299</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">4</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Sam</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">47</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Sydney</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">46681</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">5</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Dave</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">22</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">Texas</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">11843</td>
+		</tr>
+	</tbody>
+</table>
+
+<table class="database-table">
+	<tbody>
+		<tr>
+			<th colspan="100">EMPLOYEE_UIN</th>
+		</tr>
+		<tr>
+			<th style="font-family:monospace;">ID</th>
+			<th style="font-family:monospace;">UIN</th>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">1</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">57520-0440</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">2</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">49638-001</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">3</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">63550-194</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">4</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">68599-6112</td>
+		</tr>
+		<tr>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">5</td>
+			<td align="center" style="font-family:monospace;white-space:nowrap;">63868-453</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+
+<p>&nbsp;</p>
+
+<p><strong>Sample Output</strong></p>
+
+<pre>63868-453 Dave
+63550-194 Mary
+57520-0440 Sherrie</pre>
+
+<p>&nbsp;</p>
+
+<p>&nbsp;</p>
+
+<p><strong>Explanation</strong></p>
+
+<ul>
+	<li>
+<em>Sherrie</em>&nbsp;is <em>23</em> years old and has UIN <em>57520-0440</em>.&nbsp; This record is printed.</li>
+	<li>
+<em>Paul</em>&nbsp;is <em>30</em> years old&nbsp;and has UIN <em>49638-001</em>.&nbsp; This record is not printed.</li>
+	<li><i>A similar analysis is done on the remaining records.</i></li>
+</ul>
+
+<p>None of the three names of people less than <em>25</em> years old is repeated, so print them in alphabetical order.&nbsp; There is no additional sorting by&nbsp;<em>ID</em>&nbsp;in this case.</p>
+</details>
+</div>
+</div></div></section></div>
+*/
+/*
+Enter your query here.
+*/
+SELECT emu.UIN,
+    emp.NAME
+FROM EMPLOYEE emp 
+JOIN EMPLOYEE_UIN emu ON emp.ID = emu.ID
+WHERE AGE < 25
+ORDER BY emp.NAME, EMP.ID;
